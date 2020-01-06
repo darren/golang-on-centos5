26549b41941baf16f2ad52bc12f2ec2a1326d76f
Darren Hoo <darren.hoo@gmail.com>
2020-01-06 23:01:09 +0800

    Switch to  https://github.com/darren/centos5-go-bootstrap

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,5 @@
-FROM centos:5.11
+FROM darrenhoo/centos5-go-bootstrap:latest
 
-RUN rm -f /etc/yum.repos.d/*.repo
-RUN sed -i 's/enabled=1/enabled=0/' /etc/yum/pluginconf.d/fastestmirror.conf 
-
-COPY CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo
-RUN yum install -y gcc xz
-
-ADD go1.4.3 /usr/local/go1.4.3
 ADD go1.13.4 /usr/local/go
-
-ENV GOROOT_BOOTSTRAP=/usr/local/go1.4.3/
 RUN cd /usr/local/go/src/; ./make.bash
 RUN cd /usr/local/; rm -rf go/pkg/obj; tar cf - go | xz -z - > /go1.13.4-CentOS5.linux-amd64.tar.xz
--- build.sh
+++ build.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
-if [[ ! -r go1.4.3 ]]; then
-	wget -c https://dl.google.com/go/go1.4.3.linux-amd64.tar.gz
-	tar zxf go1.4.3.linux-amd64.tar.gz
-	mv go go1.4.3
-fi
-
 VERSION=1.13.4
 ARTIFACT=go${VERSION}-CentOS5.linux-amd64.tar.xz
+IMAGE_TAG=centos5-go${VERSION}
 
 if [[ ! -r go${VERSION}.src.tar.gz ]]; then
 	wget https://dl.google.com/go/go${VERSION}.src.tar.gz
@@ -22,7 +17,8 @@ tar zxf go${VERSION}.src.tar.gz
 mv go go${VERSION}
 patch -p1 -d go${VERSION} < go${VERSION}-fix.diff
 
-docker build .
+docker build . -t $IMAGE_TAG
+docker run -d -t $IMAGE_TAG /bin/bash 
 CONTAINER_ID=$(docker ps -alq)
 docker cp $CONTAINER_ID:/$ARTIFACT .
 
