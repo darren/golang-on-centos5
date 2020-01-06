635cfd6acdc3e9f6414966cb9dc0663598ba46b8
Darren Hoo <darren.hoo@gmail.com>
2020-01-06 22:12:06 +0800

    copy built artifact to host
    Update #3

--- build.sh
+++ build.sh
@@ -6,17 +6,28 @@ if [[ ! -r go1.4.3 ]]; then
 	mv go go1.4.3
 fi
 
-if [[ ! -r go1.13.4.src.tar.gz ]]; then
-	wget https://dl.google.com/go/go1.13.4.src.tar.gz
+VERSION=1.13.4
+ARTIFACT=go${VERSION}-CentOS5.linux-amd64.tar.xz
+
+if [[ ! -r go${VERSION}.src.tar.gz ]]; then
+	wget https://dl.google.com/go/go${VERSION}.src.tar.gz
 fi
 
-if [[ -r go1.13.4 ]]; then
+if [[ -r go${VERSION} ]]; then
   echo "Clean files"
-  rm -rf go1.13.4
+  rm -rf go${VERSION}
 fi
 
-tar zxf go1.13.4.src.tar.gz
-mv go go1.13.4
-patch -p1 -d go1.13.4 < go1.13.4-fix.diff
+tar zxf go${VERSION}.src.tar.gz
+mv go go${VERSION}
+patch -p1 -d go${VERSION} < go${VERSION}-fix.diff
 
 docker build .
+CONTAINER_ID=$(docker ps -alq)
+docker cp $CONTAINER_ID:/$ARTIFACT .
+
+if [[ -r $ARTIFACT ]]; then
+	echo "Build output: $ARTIFACT"
+else 
+	echo "Build failed"
+fi
